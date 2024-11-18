3ca8c164369a05887f627b889159a7cf732fb8c1
zxyang4 <112927988+zxyang4@users.noreply.github.com>
2024-11-18 11:10:19 +0800

    add column button

--- owner_panel/fetch-data/fetch-teachers.php
+++ owner_panel/fetch-data/fetch-teachers.php
@@ -1,19 +1,27 @@
 <?php
- include("../../assets/config.php");
- $sql="select * from teachers";
- $result=mysqli_query($conn,$sql);
- if(mysqli_num_rows($result)>0){
- 	while($row=mysqli_fetch_assoc($result)){
- 		echo "<tr>
-      <th scope='row'>".$row['s_no']."</th>
-      <td>".$row['fname']."  ".$row['lname']."</td>
-      <td>".$row['gender']."</td>
-      <td><a href='modal-teacher.php?id=". $row['id'] ."'><button id='view-more' data-id='".$row['id']."' style='height: 35px; width: 100px; background-color: skyblue; color: white; border: none; border-radius: 8px; text-decoration: none;'>View More</button>
+include("../../assets/config.php");
 
-          </a>
-      </td>
-    </tr>";
- 	}
- }
+$sql = "SELECT * FROM teachers";
+$result = mysqli_query($conn, $sql);
 
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <th scope='row'>" . $row['s_no'] . "</th>
+                <td>" . $row['fname'] . " " . $row['lname'] . "</td>
+                <td>" . $row['gender'] . "</td>
+                <td><a href='modal-teacher.php?id=" . $row['id'] . "'>
+                    <button id='view-more' data-id='" . $row['id'] . "' style='height: 35px; width: 100px; background-color: skyblue; color: white; border: none; border-radius: 8px;'>View More</button>
+                </a></td>
+                <td>
+                    <button class='btn btn-primary change-role-btn' data-id='" . $row['id'] . "' 
+                    style='height: 35px; width: 150px; background-color: skyblue; color: white; border: none; border-radius: 8px;'>Change to Admin</button>
+                </td>
+            </tr>";
+    }
+} else {
+    echo "<tr><td colspan='5' class='text-center'>No teachers found.</td></tr>";
+}
+
+mysqli_close($conn);
 ?>
