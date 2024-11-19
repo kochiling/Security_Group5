f3b78d73754efa243b36a0fb5cc29745ed569383
kochiling <112928080+kochiling@users.noreply.github.com>
2024-11-19 22:25:03 +0800

    Update fetch-student.php

--- owner_panel/fetch-data/fetch-student.php
+++ owner_panel/fetch-data/fetch-student.php
@@ -5,13 +5,12 @@
  if(mysqli_num_rows($result)>0){
     while($row=mysqli_fetch_assoc($result)){
         echo "<tr>
-      <th scope='row'>".$row['s_no']."</th>
-      <td>".$row['fname']."  ".$row['lname']."</td>
-      <td>".$row['class']." ".$row['section']."</td>
-      <td><a href='modal-student.php?id=". $row['id'] ."'><button id='view-more' data-id='".$row['id']."' style='height: 35px; width: 100px; background-color: skyblue; color: white; border: none; border-radius: 8px; text-decoration: none;'>View More</button>
-
+        <th scope='row'>" . htmlspecialchars($row['s_no']) . "</th>
+        <td>" . htmlspecialchars($row['fname']) . " " . htmlspecialchars($row['lname']) . "</td>
+        <td>" . htmlspecialchars($row['class']) . " " . htmlspecialchars($row['section']) . "</td>
+        <td><a href='modal-student.php?id=" . htmlspecialchars($row['id']) . "'><button id='view-more' data-id='" . htmlspecialchars($row['id']) . "' style='height: 35px; width: 100px; background-color: skyblue; color: white; border: none; border-radius: 8px; text-decoration: none;'>View More</button>
           </a>
-      </td>
+       </td>
     </tr>";
     }
  }
